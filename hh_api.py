cf9d864e3bbb5720fe419df8eae53629cc11346c
mikhayluv <mikhaylov.david12@gmail.com>
2024-04-13 02:44:07 +0300

    still working on API HH

--- hh_api.py
+++ hh_api.py
@@ -1,33 +1,37 @@
 import requests
 import json
 import database
+import time
 
 conn = database.create_connection('skillnestbot.db')
 
 
 def get_list_id_vacancies(text):
+
     url_list = 'https://api.hh.ru/vacancies'
     list_id = []
-    params = {'text': text, 'per_page': 100}
+    params = {'text': text}
     r = requests.get(url_list, params=params)
-    data = json.loads(r.text)
-    found = data.get('found', 0)
-    print(f'found: {found}')
+    found = json.loads(r.text)['found']
 
-    if found <= 1000:
-        pages = (found + 99) // 100
-    else:
-        found = 1000
-        pages = 10
-        print("Количество найденных вакансий больше 1000. Ограничено до 1000.")
-
-    for page in range(pages):
-        params['page'] = page
+    if found <= 100:
+        params['per_page'] = found
         r = requests.get(url_list, params=params)
-        data = json.loads(r.text)
-        items = data.get('items', [])
-        for vac in items:
+        data = json.loads(r.text)['items']
+        for vac in data:
             list_id.append(vac['id'])
+    else:
+        i = 0
+        while i <= 0:
+            params['per_page'] = 100
+            params['page'] = i
+            r = requests.get(url_list, params=params)
+            if 200 != r.status_code:
+                break
+            data = json.loads(r.text)['items']
+            for vac in data:
+                list_id.append(vac['id'])
+            i += 1
 
     return list_id
 
@@ -50,9 +54,12 @@ def get_salary(data):
         return salary_data
 
 
-def get_and_store_data(list_id):
-    for vac_id in range(200):
-        data = get_data_vacancy(list_id[vac_id])
+def get_and_store_vacancy(list_id):
+
+    count = 0
+
+    for vac_id in list_id:
+        data = get_data_vacancy(vac_id)
         vacancy_id = data.get('id')
         vacancy_name = data.get('name')
         area = data.get('area', {}).get('name')
@@ -61,21 +68,51 @@ def get_and_store_data(list_id):
         salary_to = salary_data.get('to')
         currency = salary_data.get('currency')
         description = data.get('description')
+
         database.add_vacancy_data(conn, vacancy_id, vacancy_name, area, salary_from, salary_to, currency, description)
+
+        print(f'iteration: {count}')
+        print(f'vacancy_id: {vacancy_id}')
+        print(f'vacancy_name: {vacancy_name}')
+
+        count += 1
+        if count % 29 == 0:
+            time.sleep(5)
+
+
+def get_and_store_key_skills(list_id):
+    for vac_id in list_id:
+        data = get_data_vacancy(vac_id)
+        vacancy_id = data.get('id')
+        vacancy_name = data.get('name')
+        key_skills = [skill['name'] for skill in data.get('key_skills', [])]
+        if key_skills is not None:
+            vacancies_data = [(vacancy_id, vacancy_name, key_skills)]
+            print(f'vacancies_data: {vacancies_data}')
+            # database.add_keys_kills_data(conn, vacancies_data[0], vacancies_data[1], vacancies_data[2])
+        else:
+            continue
+
+        # if key_skills:
+        #     for skill in key_skills:
+        #         database.add_keys_kills_data(conn, vacancy_id, vacancy_name, skill)
+
+
 
 
 def main():
     database.drop_table(conn, 'vacancy_data')
     database.create_vacancy_data_table(conn)
-    text_search = 'Junior Data Scientist'
+    text_search = 'junior data science'
     list_id = get_list_id_vacancies(text_search)
+    get_and_store_key_skills(list_id)
     # for key, value in enumerate(list_id):
     #     print(f"index: {key+1}, id: {value}")
     # print(list_id[120])
     # id = 96278252
     # null_count = list_id.count(None)
     # print("Количество значений None в массиве:", null_count)
-    get_and_store_data(list_id)
+    # get_and_store_vacancy(list_id)
 
 
 main()
